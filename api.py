427a021e50129f6715af5581c77a99fde3b30cfd
RadarFr <realspengui@gmail.com>
2025-07-04 16:43:46 +0100

    Update api.py

--- api.py
+++ api.py
@@ -1,20 +1,19 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 import humanize
-import platform
 import os
 import time
 import psutil
 import pynvml
+import sys
+import subprocess
 
-os_name = str(platform.system()).capitalize()
-amd_intilized = False
+cpu_name = subprocess.run(["powershell", "-Command", "Get-CimInstance -ClassName Win32_Processor | Select-Object -ExpandProperty Name"],capture_output=True, text=True).stdout.strip()
+is_windows = hasattr(sys, 'getwindowsversion') 
 
-if os_name == "LINUX":
+if is_windows == False:
     import amdsmi
     from amdsmi import *
-    print("You are running Linux, added AMD support has been initialized")
-    amd_intilized = True
 
 def getCoreUsage():
     time.sleep(1)
@@ -32,13 +31,15 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
-    allow_credentials=False,
-    allow_methods=["GET"],
-    allow_headers=["*"],
+    allow_origins=["*"],  # Allows all origins
+    allow_credentials=True,
+    allow_methods=["*"],  # Allows all methods (GET, POST, etc.)
+    allow_headers=["*"],  # Allows all headers
 )
 
 ifHasNvidaGPU = True # defaults to NVIDA GPU
+if is_windows == False:
+    ifHasAMDGPU = False
 
 try:
     import pynvml
@@ -46,8 +47,8 @@ try:
 except Exception:
     ifHasNvidaGPU = False
 
-if amd_intilized == True:
-    ifHasAMDGPU = False #initialized when amd lib is imported
+
+if ifHasNvidaGPU == False and is_windows == False:
     try:
         if ifHasNvidaGPU == False:
             amdsmi.init()
@@ -57,31 +58,30 @@ if amd_intilized == True:
     except Exception:
         ifHasAMDGPU = False
 
+pynvml.nvmlInit()
+
 @app.get("/stats")
 def send_stats():
     response = {
-        "SYSTEM":{
-            "os_name": os.getlogin(),
-            "os_system": os.system(),
-            "os_version": os.version(),
-        },
+        "win_username": os.getlogin(),
+        "status": "Online",
         "CPU": {
             "frequency": round(psutil.cpu_freq().current / 1000, 1),
             "utilization": psutil.cpu_percent(),
             "cores": psutil.cpu_count(),
-            "temp":"Not Available",
+            "name":str(cpu_name)
         },
-        "MEMORY": {
+        "Memory": {
             "utilization": psutil.virtual_memory().percent,
             "total": humanize.naturalsize(psutil.virtual_memory().total, binary=True),
             "free": humanize.naturalsize(psutil.virtual_memory().free, binary=True),
             "used": humanize.naturalsize(psutil.virtual_memory().used, binary=True),
         },
-        "DISK": {
-            "utilization": humanize.naturalsize(psutil.disk_usage("C:").used),
-            "free": humanize.naturalsize(psutil.disk_usage("C:").free),
-            "total": humanize.naturalsize(psutil.disk_usage("C:").total),
-            "percent": psutil.disk_usage("C:").percent,
+        "Disk": {
+            "utilization": humanize.naturalsize(psutil.disk_usage("C://").used),
+            "free": humanize.naturalsize(psutil.disk_usage("C://").free),
+            "total": humanize.naturalsize(psutil.disk_usage("C://").total),
+            "percent": psutil.disk_usage("C://").percent,
         }
     }
 
@@ -105,28 +105,28 @@ def send_stats():
             response["GPU"] = {"error": str(e)}
     else:
         response["GPU"] = "No GPU available"
-
-    if ifHasAMDGPU and amd_intilized:
-        try:
-            devices = amdsmi.amdsmi_get_processor_handles()
-            if len(devices) == 0:
-                response["GPU"] = "No GPU available"
-            else:
-                for device in devices:
-                    vram_usage = amdsmi.amdsmi_get_gpu_vram_usage(device)
-                    response["GPU"] = {
-                        "gpu_status": ifHasAMDGPU,
-                        "name": ,
-                        "temperature": ,
-                        "utilization": ,
-                        "mem_free": vram_usage[""],
-                        "mem_used": vram_usage["vram_used"],
-                        "mem_total": vram_usage["vram_total"],
-                        "power_usage": ,
-                    }
-        except Exception as e:
-            response["GPU"] = {"error": str(e)}
-
+    if is_windows == False:
+        if ifHasAMDGPU:
+            try:
+                devices = amdsmi.amdsmi_get_processor_handles()
+                if len(devices) == 0:
+                    response["GPU"] = "No GPU available"
+                else:
+                    for device in devices:
+                        vram_usage = amdsmi.amdsmi_get_gpu_vram_usage(device)
+                        response["GPU"] = {
+                            "gpu_status": ifHasAMDGPU,
+                            #"name": ,
+                            #"temperature": ,
+                            #"utilization": ,
+                            "mem_free": vram_usage[""],
+                            "mem_used": vram_usage["vram_used"],
+                            "mem_total": vram_usage["vram_total"],
+                            #"power_usage": ,
+                        }
+            except Exception as e:
+                response["GPU"] = {"error": str(e)}
+        
     return response
 
 @app.get("/SlowerStats")
@@ -138,5 +138,5 @@ def send_SlowerStats():
        }
     }
     return responce
-
+  
 # Run with: uvicorn api:app --host 0.0.0.0 --port 8000 --reload
